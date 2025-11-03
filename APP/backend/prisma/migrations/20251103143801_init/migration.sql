a0b7d019ffe691814e648c9be227dc775c91f61b
paucean_ioana <166832687+ioana333@users.noreply.github.com>
2025-11-03 23:03:38 +0200

    backend init

---
+++ APP/backend/prisma/migrations/20251103143801_init/migration.sql
@@ -0,0 +1,48 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "email" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "city" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Event" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "city" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
+    "category" TEXT NOT NULL,
+    "venue" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Event_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Wishlist" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "eventId" INTEGER NOT NULL,
+
+    CONSTRAINT "Wishlist_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Wishlist_userId_eventId_key" ON "Wishlist"("userId", "eventId");
+
+-- AddForeignKey
+ALTER TABLE "Wishlist" ADD CONSTRAINT "Wishlist_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Wishlist" ADD CONSTRAINT "Wishlist_eventId_fkey" FOREIGN KEY ("eventId") REFERENCES "Event"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ APP/backend/prisma/seed.ts
@@ -0,0 +1,13 @@
+﻿import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+async function main() {
+  const events = [
+    { title: "Concert Rock", city: "Cluj", date: new Date("2025-12-05"), category: "Muzică", venue: "Form Space" },
+    { title: "Stand-up", city: "București", date: new Date("2025-12-06"), category: "Comedie", venue: "Sala Gloria" },
+    { title: "Târg de Crăciun", city: "Brașov", date: new Date("2025-12-07"), category: "Târg", venue: "Piața Sfatului" },
+  ];
+  await prisma.event.createMany({ data: events, skipDuplicates: true });
+  console.log("Seed OK");
+}
+main().finally(() => prisma.$disconnect());
